0c0c1ea13a999c710e0993254dd24ef284fcc5dc
Alberto Benitez <albertobenitezbahena@gmail.com>
2022-06-04 20:57:57 -0500

    Personalización del modulo purchase request

---
+++ purchase_request_custom/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "purchase request custom",
+    'version': '1.0',
+    'depends': ['purchase_request'],
+    'author': "Daten S.A.S.",
+    'category': 'compras',
+    'description': "oculta campo grupo de abastecimiento",
+    # data files always loaded at installation
+    'data': ["views/inherited_purchase_request.xml",],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+}
