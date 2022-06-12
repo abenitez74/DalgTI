dc9f74c3058fb858772c00efa1fb63db2fdbe896
Alberto Benitez <albertobenitezbahena@gmail.com>
2022-06-11 20:44:57 -0500

    Ocultar boton crear factura

--- purchase_request_custom/__manifest__.py
+++ purchase_request_custom/__manifest__.py
@@ -6,7 +6,9 @@
     'category': 'compras',
     'description': "oculta campo grupo de abastecimiento",
     # data files always loaded at installation
-    'data': ["views/inherited_purchase_request.xml",],
+    'data': ["views/inherited_purchase_request.xml",
+             "views/inherited_purchase_order.xml",
+            ],
     # data files containing optionally loaded demonstration data
     'demo': [],
     "license": "LGPL-3",
